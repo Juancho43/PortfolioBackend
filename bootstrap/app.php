c4ccd264ec87eb8ab18d1a5466da75c5a6f48678
Juan Bravo <bravo43juancho@gmail.com>
2025-06-09 22:00:31 -0300

    Add comments to clarify middleware usage in app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,7 +13,20 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->middleware[] = \App\Http\Middleware\Authenticate::class; 
+        // Para rutas API específicamente
+        $middleware->api(append: [
+            \App\Http\Middleware\Authenticate::class,
+        ]);
+
+        // O si quieres para rutas web:
+        // $middleware->web(append: [
+        //     \App\Http\Middleware\Authenticate::class,
+        // ]);
+
+        // O si quieres para todas las rutas (global):
+        // $middleware->append([
+        //     \App\Http\Middleware\Authenticate::class,
+        // ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
